1a7280edfb13163b741bab5b04e6ea76c6af7cff
Kai Moore <haohao12012001@gmail.com>
2022-05-03 23:30:27 +0700

    fix git

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-import './graphql';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-mongoose.connect(process.env.MONGO_URI || "")
-  .then(() => console.log(`Successfully connect to mongodb`))
-  .catch(error => console.log(error));  
